1477278451f673c7ffab33b82a9c4e4a74bde1cb
heimao0307 <fengyun86868@163.com>
2017-07-13 10:23:01 +0800

    first commit

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# 现在我要开始数我的鸡了
+print ("I will now count my chickens")
+#母鸡和公鸡的数量
+print ("Hens" ,25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+#我应该怎么算鸡蛋的数量呢
+print ("Now I will count the eggs :")
+#算出鸡蛋的和数量
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#3+2小于5-7吗？
+print ("Is it true that 3 + 2 < 5 - 7 ？")
+print (3 + 2 < 5 - 7)
+#3+2是多少
+print ("What is 3 + 2 ?" , 3 + 2 )
+#5—7是多少
+print ("What is 5 - 7 ?" , 5 - 7 ) 
+#噢，这就是为什么它是错误的
+print ("Oh, that's why it's False.")
+#再多来几个吧
+print ("How about some more.")
+#5>-2吗
+print ("Is it greater?", 5 > -2)
+#5>=-2吗
+print("Is it greater or equal?", 5 >=-2)
+#5<=-2吗
+print("Is it less or equal?", 5 <= -2) 
+
+ 
